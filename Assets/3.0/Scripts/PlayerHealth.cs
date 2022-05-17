110249e9a37e5dcfa33fa8d95880b1a94132a824
Motinlin <96507966+Motinlin@users.noreply.github.com>
2022-05-17 11:54:55 +0800

    3.0

---
+++ Assets/3.0/Scripts/PlayerHealth.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private SpriteRenderer healthBar;
+    public float health = 100f;
+    public float repeatDamagePeriod = 2f;//无敌时间
+    public float hurtForce = 10f;
+    public float damageAmount = 10f;
+
+    private float lastHitTime;
+    private Vector3 healthScale;
+    private HeroController playerControl;
+    private Animator anim;
+    void Awake()
+    {
+        playerControl = GetComponent<HeroController>();
+        healthBar = GameObject.Find("HealthBar").GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        healthScale = healthBar.transform.localScale;
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            //可以再次减血
+            if (Time.time > lastHitTime + repeatDamagePeriod)
+            {
+                if (health > 0f)
+                {
+                    TakeDamage(col.transform);
+                    lastHitTime = Time.time;
+                }
+               
+            }
+        }
+    }
+    void death()
+    {
+        Collider2D[] cols = GetComponents<Collider2D>();
+        foreach (Collider2D c in cols)
+        {
+            c.isTrigger = true;
+        }
+
+        SpriteRenderer[] spr = GetComponentsInChildren<SpriteRenderer>();
+        foreach (SpriteRenderer s in spr)
+        {
+            s.sortingLayerName = "UI";
+        }
+
+        //GetComponent<PlayerCtrl>().enabled = false;
+        playerControl.enabled = false;//不能再向角色输入指令
+        GetComponentInChildren<Gun>().enabled = false;
+        anim.SetTrigger("Die");
+
+        //销毁血条
+        GameObject go = GameObject.Find("Blood");
+        Destroy(go);
+    }
+    void TakeDamage(Transform enemy)
+    {
+        playerControl.bJump = false;
+        Vector3 hurtVector = transform.position - enemy.position + Vector3.up * 10f;
+        GetComponent<Rigidbody2D>().AddForce(hurtVector * hurtForce);
+        health -= damageAmount;
+        if (health <= 0)
+        {
+            death();
+            return;//死亡之后不必再更新血条
+        }
+           
+        UpdateHealthBar();
+    }
+
+    public void UpdateHealthBar()
+    {
+        healthBar.material.color = Color.Lerp(Color.green, Color.red, 1 - health * 0.01f);
+        healthBar.transform.localScale = new Vector3(healthScale.x * health * 0.01f, 1.5f, 1);
+    }
+
+}
---
+++ Assets/3.0/Scripts/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject explosion;
+    void Start()
+    {
+        //Destroy(gameObject, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag != "Player")
+        {
+            float rotation = Random.Range(0, 360);
+            Instantiate(explosion, transform.position,
+                        Quaternion.Euler(0, 0, rotation));//旋转的是爆炸动画，不是火箭
+            Destroy(gameObject);
+        }
+        if(collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<Enemy>().Hurt();
+            //当碰到的是敌人，则调用Enemy的Hurt使得敌人HP--；
+        }
+    }
+}
